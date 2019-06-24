428a6a74d6f94bc60e856074315e47f5b61f15ed
akiyoko <akiyoko@users.noreply.github.com>
2019-06-25 05:35:12 +0900

    Fix tests

--- day3/test_calculate.py
+++ day3/test_calculate.py
@@ -1,9 +1,9 @@
 import unittest
 
-from day3.calculate import square
+from day3.calculate import square, divide
 
 
-class TestCalculate(unittest.TestCase):
+class TestSquare(unittest.TestCase):
     def test_calculate_success(self):
         self.assertEqual(square(0), 0)
         self.assertEqual(square(1), 1)
@@ -14,3 +14,23 @@ class TestCalculate(unittest.TestCase):
 
     def test_calculate_if_str(self):
         self.assertRaises(TypeError, square, 'a')
+
+
+class TestDivide(unittest.TestCase):
+    def test_calculate_success(self):
+        self.assertEqual(divide(6, 2), 3)
+
+    def test_calculate_if_x_none(self):
+        self.assertRaises(TypeError, divide, None, 2)
+
+    def test_calculate_if_x_str(self):
+        self.assertRaises(TypeError, divide, 'a', 2)
+
+    def test_calculate_if_y_none(self):
+        self.assertRaises(TypeError, divide, 6, None)
+
+    def test_calculate_if_y_str(self):
+        self.assertRaises(TypeError, divide, 6, 'a')
+
+    def test_calculate_if_y_zero(self):
+        self.assertRaises(TypeError, divide, 6, 0)

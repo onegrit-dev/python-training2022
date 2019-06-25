906fc292b74bdf88894d88965bfb9b99af574ecb
akiyoko <akiyoko@users.noreply.github.com>
2019-06-25 21:20:31 +0900

    Fix some

--- day3/test_calculate.py
+++ day3/test_calculate.py
@@ -4,16 +4,19 @@ from day3.calculate import square, divide
 
 
 class TestSquare(unittest.TestCase):
-    def test_calculate_success(self):
-        self.assertEqual(square(0), 0)
-        self.assertEqual(square(1), 1)
-        self.assertEqual(square(2), 4)
 
-    def test_calculate_if_none(self):
-        self.assertRaises(TypeError, square, None)
+    def test_square_success(self):
+        self.assertEqual(0, square(0))
+        self.assertEqual(1, square(1))
+        self.assertEqual(4, square(2))
 
-    def test_calculate_if_str(self):
-        self.assertRaises(TypeError, square, 'a')
+    def test_square_if_none(self):
+        with self.assertRaises(TypeError):
+            square(None)
+
+    def test_square_if_str(self):
+        with self.assertRaises(TypeError):
+            square('a')
 
 
 class TestDivide(unittest.TestCase):

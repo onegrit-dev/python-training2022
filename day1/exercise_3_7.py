cd8398e4315fa50b3dc68d51c22da7383fee09cd
akiyoko <akiyoko@users.noreply.github.com>
2022-05-16 01:53:27 +0900

    Fix

--- day1/exercise_3_7.py
+++ day1/exercise_3_7.py
@@ -15,8 +15,8 @@ print(str(a) + b)
 【実行結果】
 
 Traceback (most recent call last):
-  File "/Users/akiyoko/PycharmProjects/training1906/day1/exercise_3_7.py", line 7, in <module>
-    print(c + d)
+  File "C:/Users/akiyoko/PycharmProjects/training2022/day1/exercise_3_7.py", line 3, in <module>
+    print(a + b)
 TypeError: unsupported operand type(s) for +: 'int' and 'str'
 """
 

16bdbe1a7fd0de42cdd80ef98e2532234c01a6c4
akiyoko <akiyoko@users.noreply.github.com>
2022-05-17 02:19:25 +0900

    Add

---
+++ day3/exercise_3_1.py
@@ -0,0 +1,17 @@
+hands = {1: 'グー', 2: 'チョキ', 3: 'パー'}
+your_hand = None
+
+while True:
+    input_value = input("""1から3の数字を選択してください。
+1: グー
+2: チョキ
+3: パー
+> """)
+    try:
+        input_value = int(input_value)
+        your_hand = hands[input_value]
+        break
+    except (ValueError, KeyError):
+        continue
+
+print('あなたは{}を選択しました。'.format(your_hand))

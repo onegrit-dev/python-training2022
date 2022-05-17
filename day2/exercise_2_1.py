35df1a263c318e246aa4ee5542373e5a6c572a8d
akiyoko <akiyoko@users.noreply.github.com>
2022-05-17 15:15:53 +0900

    Fix

--- day2/exercise_2_1.py
+++ day2/exercise_2_1.py
@@ -1,9 +1,29 @@
-words = ['This', 'is', 'Python', 3]
-for word in words:
-    print(word, end=' ')
+i = 0
+while i < 5:
+    print(i)
+    i += 1
 
 """
 【実行結果】
 
-This is Python 3 
+0
+1
+2
+3
+4
+"""
+
+i = 0
+while i < 5:
+    i += 1
+    print(i)
+
+"""
+【実行結果】
+
+1
+2
+3
+4
+5
 """

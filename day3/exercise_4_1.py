e3e531f1f6387d242d64b8597523c0e6a613e45f
akiyoko <akiyoko@users.noreply.github.com>
2022-05-20 03:37:01 +0900

    Fix

--- day3/exercise_4_1.py
+++ day3/exercise_4_1.py
@@ -1,10 +1,3 @@
-f = open('sample.txt')
-for line in f:
-    print(line)
-f.close()
-
-# あるいは with文を使って
-
-with open('sample.txt') as f:
+with open('sample2.txt') as f:
     for line in f:
         print(line)

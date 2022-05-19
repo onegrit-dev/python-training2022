52c54b3294534479927bff1ab5539df37da864b1
akiyoko <akiyoko@users.noreply.github.com>
2022-05-19 11:02:45 +0900

    Fix

--- day3/exercise_4_3.py
+++ day3/exercise_4_3.py
@@ -1,10 +1,3 @@
-import os.path
-
-if not os.path.exists('sample.txt'):
-    raise Exception('ファイルが存在しません。')
-
-# あるいは pathlib を使って
-
 from pathlib import Path
 
 if not Path.exists(Path('sample.txt')):

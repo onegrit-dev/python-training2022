374ef179d2d6aa69d3b6fd5dd67fce305aa8daf8
akiyoko <akiyoko@users.noreply.github.com>
2022-05-20 04:34:05 +0900

    Fix

--- day3/exercise_2_2.py
+++ day3/exercise_2_2.py
@@ -1,9 +1,14 @@
-import os
+import random
 
-print(os.getcwd())
+
+def draw_fortune():
+    return random.choice(['大吉', '吉', '小吉', '末吉', '凶'])
+
+
+print(draw_fortune())
 
 """
 【実行結果】（例）
 
-C:¥Users¥akiyoko¥PycharmProjects¥training2022¥day3
+凶
 """
--- day3/exercise_2_3.py
+++ day3/exercise_2_3.py
@@ -1,37 +1,13 @@
-import random
+import datetime
 
+now = datetime.datetime.now()
+print(now)
 
-def draw_fortune():
-    return random.choice(['大吉', '吉', '小吉', '末吉', '凶'])
-
-
-def draw_fortune2():
-    x = random.randint(0, 9)
-    print(x)
-    if x == 9:
-        # 9
-        return '大吉'
-    elif x >= 6:
-        # 6, 7, 8
-        return '吉'
-    elif x >= 4:
-        # 4, 5
-        return '小吉'
-    elif x >= 2:
-        # 2, 3
-        return '末吉'
-    else:
-        # 0, 1
-        return '凶'
-
-
-print(draw_fortune())
-print(draw_fortune2())
+print(now.strftime('%Y/%m/%d %H:%M:%S'))
 
 """
 【実行結果】（例）
 
-凶
-1
-凶
+2022-05-20 13:50:03.675601
+2022/05/20 13:51:09
 """

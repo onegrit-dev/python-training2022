58b50655009df8455c2cd05fbb9104d7216c4a0c
akiyoko <akiyoko@users.noreply.github.com>
2022-05-20 05:13:15 +0900

    Add

---
+++ day3/exercise_2_5.py
@@ -0,0 +1,12 @@
+import openpyxl
+
+# Workbookオブジェクトを新規作成
+wb = openpyxl.Workbook()
+# 1つ目のシートをWorksheetオブジェクトとして取得
+ws = wb.worksheets[0]
+
+# セルA1に値を書き込む
+ws['A1'] = 'パイソン'
+
+# Excelファイルを保存
+wb.save('Book2.xlsx')
